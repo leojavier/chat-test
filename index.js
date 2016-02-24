e3ac533bc172766bf41e3d564ecf0069db2c489d
leojavier <info@leojavier.com>
2016-02-24 18:18:11 -0500

    adding port

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-server.listen(3000);
+var port = Number(process.env.PORT || 3000);
+server.listen(port);
 
 app.get('/', function(req,res) {
   res.sendfile(__dirname + '/index.html');
